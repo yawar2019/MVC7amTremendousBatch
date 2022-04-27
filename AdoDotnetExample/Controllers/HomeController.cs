5029977ddc0accd12ca095ae6f20d9776385547e
yawar2019 <yawarali17@gmail.com>
2022-04-27 08:00:49 +0530

    committed

---
+++ AdoDotnetExample/Controllers/HomeController.cs
@@ -0,0 +1,32 @@
+﻿using AdoDotnetExample.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace AdoDotnetExample.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        EmployeeContext db = new EmployeeContext();
+
+        public ActionResult Index()
+        {
+           
+            return View(db.GetEmployees());
+        }
+
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Create(string EmpName,int EmpSalary)
+        {
+            return View();
+        }
+    }
+}
