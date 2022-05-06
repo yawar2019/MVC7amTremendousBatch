20898cf9e0504a31d25d3e6d057ae9bfe56c6940
yawar2019 <yawarali17@gmail.com>
2022-05-06 08:00:56 +0530

    committed

---
+++ DatabaseApproach/Controllers/EmployeeController.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using DatabaseApproach.Models;
+
+namespace DatabaseApproach.Controllers
+{
+    public class EmployeeController : Controller
+    {
+        private EmployeeDbEntities1 db = new EmployeeDbEntities1();
+
+        // GET: Employee
+        public ActionResult Index()
+        {
+            return View(db.tbl_Employee.ToList());
+        }
+
+        // GET: Employee/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            tbl_Employee tbl_Employee = db.tbl_Employee.Find(id);
+            if (tbl_Employee == null)
+            {
+                return HttpNotFound();
+            }
+            return View(tbl_Employee);
+        }
+
+        // GET: Employee/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Employee/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "EmpId,EmpName,EmpSalary")] tbl_Employee tbl_Employee)
+        {
+            if (ModelState.IsValid)
+            {
+                db.tbl_Employee.Add(tbl_Employee);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(tbl_Employee);
+        }
+
+        // GET: Employee/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            tbl_Employee tbl_Employee = db.tbl_Employee.Find(id);
+            if (tbl_Employee == null)
+            {
+                return HttpNotFound();
+            }
+            return View(tbl_Employee);
+        }
+
+        // POST: Employee/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "EmpId,EmpName,EmpSalary")] tbl_Employee tbl_Employee)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(tbl_Employee).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(tbl_Employee);
+        }
+
+        // GET: Employee/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            tbl_Employee tbl_Employee = db.tbl_Employee.Find(id);
+            if (tbl_Employee == null)
+            {
+                return HttpNotFound();
+            }
+            return View(tbl_Employee);
+        }
+
+        // POST: Employee/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            tbl_Employee tbl_Employee = db.tbl_Employee.Find(id);
+            db.tbl_Employee.Remove(tbl_Employee);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
